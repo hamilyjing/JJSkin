264702a4355939a5119bec165ffadfd56e4258de
JJ <gongjian_001@126.com>
2015-06-01 15:30:04 +0800

    Initial Commit

---
+++ JJSkin/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  JJSkin
+//
+//  Created by gongjian03 on 6/1/15.
+//  Copyright (c) 2015 gongjian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
