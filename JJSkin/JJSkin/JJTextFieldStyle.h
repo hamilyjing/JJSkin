913d92c8959d9f3853ef6a1099a541d7d2dae4d5
JJ <gongjian_001@126.com>
2015-06-08 15:08:56 +0800

    Add text field style

---
+++ JJSkin/JJSkin/JJTextFieldStyle.h
@@ -0,0 +1,33 @@
+//
+//  JJTextFieldStyle.h
+//  JJSkin
+//
+//  Created by gongjian03 on 6/8/15.
+//  Copyright (c) 2015 gongjian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "JJControlStyle.h"
+
+@class JJFontStyle;
+
+@interface JJTextFieldStyle : JJControlStyle
+
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, copy) NSString *placeholder;
+@property (nonatomic, strong) JJFontStyle *font;
+@property (nonatomic, strong) UIColor *textColor;
+@property (nonatomic, copy) NSString *textAlignment; // NSTextAlignment
+
+@property (nonatomic, copy) NSString *autocapitalizationType; // UITextAutocapitalizationType
+@property (nonatomic, copy) NSString *autocorrectionType; // UITextAutocorrectionType
+@property (nonatomic, copy) NSString *spellCheckingType; // UITextSpellCheckingType
+@property (nonatomic, copy) NSString *enablesReturnKeyAutomatically;
+@property (nonatomic, copy) NSString *keyboardAppearance; // UIKeyboardAppearance
+@property (nonatomic, copy) NSString *keyboardType; // UIKeyboardType
+@property (nonatomic, copy) NSString *returnKeyType; // UIReturnKeyType
+@property (nonatomic, copy) NSString *secureTextEntry;
+
+@end
---
+++ JJSkin/JJSkin/JJTextFieldStyle.m
@@ -0,0 +1,98 @@
+//
+//  JJTextFieldStyle.m
+//  JJSkin
+//
+//  Created by gongjian03 on 6/8/15.
+//  Copyright (c) 2015 gongjian. All rights reserved.
+//
+
+#import "JJTextFieldStyle.h"
+
+#import "JJFontStyle.h"
+#import "NSObject+JJ.h"
+#import "JJSkinManager.h"
+
+@implementation JJTextFieldStyle
+
+#pragma mark - JJSkinStyleDataSource
+
++ (id)objectFromStyle:(id)style
+{
+    UITextField *textField = [[UITextField alloc] init];
+    [style updateObject:textField];
+    return textField;
+}
+
+- (void)updateObject:(id)object
+{
+    [super updateObject:object];
+    
+    UITextField *textField = (UITextField *)object;
+    
+    if (_text)
+    {
+        textField.text = _text;
+    }
+    
+    if (_placeholder)
+    {
+        textField.placeholder = _placeholder;
+    }
+    
+    if (_font)
+    {
+        textField.font = [JJFontStyle objectFromStyle:_font];
+    }
+    
+    if (_textColor)
+    {
+        textField.textColor = _textColor;
+    }
+    
+    if (_textAlignment)
+    {
+        textField.textAlignment = [JJSkinManager getIntegerFromString:_textAlignment];
+    }
+    
+    if (_autocapitalizationType)
+    {
+        textField.autocapitalizationType = [JJSkinManager getIntegerFromString:_autocapitalizationType];
+    }
+    
+    if (_autocorrectionType)
+    {
+        textField.autocorrectionType = [JJSkinManager getIntegerFromString:_autocorrectionType];
+    }
+    
+    if (_spellCheckingType)
+    {
+        textField.spellCheckingType = [JJSkinManager getIntegerFromString:_spellCheckingType];
+    }
+    
+    if (_enablesReturnKeyAutomatically)
+    {
+        textField.enablesReturnKeyAutomatically = [JJSkinManager getBoolFromString:_enablesReturnKeyAutomatically];
+    }
+    
+    if (_keyboardAppearance)
+    {
+        textField.keyboardAppearance = [JJSkinManager getIntegerFromString:_keyboardAppearance];
+    }
+    
+    if (_keyboardType)
+    {
+        textField.keyboardType = [JJSkinManager getIntegerFromString:_keyboardType];
+    }
+    
+    if (_returnKeyType)
+    {
+        textField.returnKeyType = [JJSkinManager getIntegerFromString:_returnKeyType];
+    }
+    
+    if (_secureTextEntry)
+    {
+        textField.secureTextEntry = [JJSkinManager getBoolFromString:_secureTextEntry];
+    }
+}
+
+@end
